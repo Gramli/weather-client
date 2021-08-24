416e0512ee7b8dacb2e15e7f14027ccaab0c68b7
Daniel Balcárek <daniel.balcarek@notino.com>
2021-08-24 16:32:04 +0200

    continue in implementation of CoordinatesForm

--- src/components/CoordinatesForm.tsx
+++ src/components/CoordinatesForm.tsx
@@ -1,17 +1,37 @@
+import { FormEvent, useRef, useState } from "react";
 import Container from "./ui/Container";
 
 const CoordinatesForm: React.FC = () => {
 
+    const [lat, setLat] = useState('');
+    const [long, setLong] = useState('');
+
+    const latRef = useRef<HTMLInputElement>(null);
+    const longRef = useRef<HTMLInputElement>(null);
+
+
+    const onSubmitHandler = (event: FormEvent) => {
+        event.preventDefault();
+
+        if(lat.trim() === '' || long.trim() === ''){
+            return;
+        }
+
+        //clear data at the end
+        setLat('');
+        setLong('');
+    }
+
     return <Container>
         <div>
             <label>Latitude: </label>
-            <input></input>            
+            <input ref={latRef} value={lat}></input>            
         </div>
         <div>
             <label>Longtitude:</label>
-            <input></input>  
+            <input ref={longRef} value={long}></input>  
         </div>
-        <button>Submit</button>
+        <button type="submit" >Submit</button>
     </Container>
 }
 
